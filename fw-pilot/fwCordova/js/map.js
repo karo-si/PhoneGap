3fad759435d08797ebf00c379b9f4527432ef01d
karo-si <robert.korosin@add.si>
2014-10-26 12:17:41 +0100

    New

---
+++ fw-pilot/fwCordova/js/map.js
@@ -0,0 +1,19 @@
+﻿document.addEventListener("deviceready", function () {
+    var div = document.getElementById("map_canvas");
+    if (div)
+    {
+        // embeded
+        var map = plugin.google.maps.Map.getMap(div);
+    }
+    else
+    {
+        // full
+        var map = plugin.google.maps.Map.getMap();
+    }
+    map.on(plugin.google.maps.event.MAP_READY, onMapReady);
+});
+
+function onMapReady(map) {
+    // show full screen
+    map.showDialog();
+}
